7d0c79fe9d08293cc5bd0a774b10b91d3228ef32
seonghyeok <chosh2001@naver.com>
2025-07-21 22:14:45 +0900

    chore: 불필요한 공백 제거

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -12,12 +12,12 @@ variable "instance_type" {
 
 variable "mongodb_admin_user" {
   description = "MongoDB 관리자 사용자 이름"
-  type        = string
-  sensitive   = true
+  type = string
+  sensitive = true
 }
 
 variable "mongodb_admin_password" {
   description = "MongoDB 관리자 비밀번호"
-  type        = string
-  sensitive   = true
+  type = string
+  sensitive = true
 }
